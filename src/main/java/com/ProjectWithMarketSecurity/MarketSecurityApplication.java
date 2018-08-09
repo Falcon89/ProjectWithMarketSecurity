7d9a93223ea58e5d69e90c37e7dd71bb59af09ea
falcon89 <vasyl.kachala@gmail.com>
2018-08-09 17:12:27 +0300

    Create 09.08.2018

---
+++ src/main/java/com/ProjectWithMarketSecurity/MarketSecurityApplication.java
@@ -0,0 +1,20 @@
+package com.ProjectWithMarketSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+//@EnableScheduling
+public class MarketSecurityApplication extends SpringBootServletInitializer {
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(MarketSecurityApplication.class);
+//	}
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSecurityApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ProjectWithMarketSecurity/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.ProjectWithMarketSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String login(){ return "login";}
+
+    @PostMapping("/")
+    public String indexAfterLogin() {
+        return "redirect:/";
+    }
+}
